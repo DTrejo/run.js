dc0c2cd702bbf8147d8261b911d767a6e08d5238
David Trejo <david.daniel.trejo@gmail.com>
2010-11-08 22:03:58 -0500

    github url

--- run.js
+++ run.js
@@ -1,3 +1,4 @@
+// https://github.com/DTrejo/run.js
 // Used to run code in a directory and rerun it if any files are changed.
 // usage: node run.js servercode.js
 // servercode.js is whatever js file you want to run with node.
